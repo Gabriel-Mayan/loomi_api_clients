d9ac6a99ba8e816e61e137d60549a7bc5d92cc5d
Gabriel-Mayan <gabrielmayan@gmail.com>
2024-07-27 17:14:12 -0300

    chore(misc): adicionando cors, logger e scripts básicos pro projeto

--- src/http.ts
+++ src/http.ts
@@ -1,11 +1,18 @@
+import cors from "cors";
 import helmet from "helmet";
 import express from "express";
 import { createServer } from "http";
+
+import { corsConfig } from "./config/cors.config";
+
+import logger from "./services/logger.service";
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
+app.use(cors(corsConfig));
+app.use(logger);
 
 const server = createServer(app);
 
