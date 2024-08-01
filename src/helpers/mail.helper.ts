5005fceb59c7b9ebc27930cb9ffc39888d99a173
Gabriel Mayan <gabrielmayan@gmail.com>
2024-08-01 10:45:45 -0300

    feat(misc): Criação de funções para enviar e receber eventos do kafka

--- src/helpers/mail.helper.ts
+++ src/helpers/mail.helper.ts
@@ -1,9 +1,30 @@
-// TODO
-export const recoveryPasswordMailSend = async ({ to, code }: { to: string, code: any }) => {
+import { isAfterDate } from "../utils/date.util";
+import { RecoveryPasswordRepository } from "../repositories/recovery-password.repository";
+import { InternalError, RequestFieldError } from "../services/error.service";
+import { serviceMailSender } from "../services/messagery.service";
 
+export const recoveryPasswordMailSend = async ({ to, code }: { to: string, code: any }) => {
+    await serviceMailSender({ topic: 'recovery-password', message: { to, code }});
 };
 
-// TODO
-export const recoveryPasswordMailSent = async ({ message }: { message: any }) => {
+export const recoveryPasswordMailSent = async ({ to, code, isSent }: { to: string, code: any, isSent: boolean }) => {
+    const recovery = await RecoveryPasswordRepository.findRecovery({ id: code });
 
+    if (!recovery) {
+        throw new RequestFieldError("Request does not exist...");
+    };
+
+    const expiredTime = isAfterDate(new Date(), recovery.expiresIn);
+
+    if (expiredTime) {
+        throw new RequestFieldError("Request timeout expired...");
+    };
+
+    const recoveryUpdated = await RecoveryPasswordRepository.updateRecovery({ id: recovery.id, isSent });
+
+    if (!recoveryUpdated.affected) {
+        throw new InternalError("Error when updating password...", { extras: { recovery_info: { to, code } } });
+    };
+    
+    return true;
 };
--- src/repositories/recovery-password.repository.ts
+++ src/repositories/recovery-password.repository.ts
@@ -1,4 +1,4 @@
-import { FindOptionsWhere } from "typeorm";
+import { FindOptionsWhere, UpdateResult } from "typeorm";
 
 import { RecoveryPassword } from "../entities/recovery-password.entity";
 
@@ -8,10 +8,14 @@ const repository = AppDataSource.getRepository(RecoveryPassword);
 
 export const RecoveryPasswordRepository = {
   findRecovery(query: FindOptionsWhere<RecoveryPassword>) {
-      return repository.findOneBy({ ...query });
-  },  
+    return repository.findOneBy({ ...query });
+  },
 
   createRecovery(recovery: any) {
     return repository.insert(recovery);
+  },
+
+  updateRecovery({ id, isSent }: { id: string, isSent: boolean }): Promise<UpdateResult> {
+    return repository.update({ id }, { mailSent: isSent });
   },
 };
